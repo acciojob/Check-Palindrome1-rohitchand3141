29e88c99bdeff1a04b94e6dddf4f43ef583447f7
ROHIT CHAND <100352296+rohitchand3141@users.noreply.github.com>
2023-03-28 21:03:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(str)
+{
+	var len = str.length;
+	var mid = Math.floor(len/2);
 
+	for ( var i = 0; i < mid; i++ ) 
+	{
+		if (str[i] !== str[len - 1 - i]) 
+		{
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
